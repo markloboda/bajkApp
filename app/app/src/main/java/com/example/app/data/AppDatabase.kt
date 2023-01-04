998b740adb6dbd2f1a3f2fff498957cfbe9690cf
Mark <markloboda123@gmail.com>
2023-01-04 19:01:15 +0100

    fixed a bug that made it not work

--- app/app/src/main/java/com/example/app/data/AppDatabase.kt
+++ app/app/src/main/java/com/example/app/data/AppDatabase.kt
@@ -9,13 +9,14 @@ import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.app.data.bike.Bike
 import com.example.app.data.bike.BikeDao
+import com.example.app.data.location.Location
 import com.example.app.data.location.LocationDao
 import com.example.app.data.reservation.Reservation
 import com.example.app.data.reservation.ReservationDao
 import com.example.app.data.user.User
 import com.example.app.data.user.UserDao
 
-@Database(entities = [Bike::class, User::class, Reservation::class], version = 1, exportSchema = false)
+@Database(entities = [Bike::class, Location::class, User::class, Reservation::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun bikeDao(): BikeDao
     abstract fun locationDao(): LocationDao
